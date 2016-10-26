0910c7350367a5315af7518953f1a572f9faf904
catv12 <kimbacrist@hotmail.com>
2016-10-25 21:58:40 -0300

    Pueba jenkins

--- VacaJSF/target/generated-sources/annotations/cl/revengers/entities/Login_.java
+++ VacaJSF/target/generated-sources/annotations/cl/revengers/entities/Login_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-24T17:02:14")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-10-25T21:54:46")
 @StaticMetamodel(Login.class)
 public class Login_ { 
 
