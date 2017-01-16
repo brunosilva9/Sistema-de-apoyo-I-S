482d693392846059366fc76efd08bb930f7dde1a
catv12 <kimbacrist@hotmail.com>
2017-01-16 01:44:23 -0300

    Arreglo de bugs V2

--- VacaJSF/target/generated-sources/annotations/cl/revengers/entities/Adelanto_.java
+++ VacaJSF/target/generated-sources/annotations/cl/revengers/entities/Adelanto_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-16T01:30:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-16T01:43:17")
 @StaticMetamodel(Adelanto.class)
 public class Adelanto_ { 
 
--- VacaJSF/target/generated-sources/annotations/cl/revengers/entities/Producto_.java
+++ VacaJSF/target/generated-sources/annotations/cl/revengers/entities/Producto_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-16T01:30:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-16T01:43:17")
 @StaticMetamodel(Producto.class)
 public class Producto_ { 
 
