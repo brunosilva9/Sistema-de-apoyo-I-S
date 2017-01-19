c9ec815d5e9e5485c74221f364c489482242d2e6
catv12 <kimbacrist@hotmail.com>
2017-01-19 00:22:00 -0300

    Final

--- VacaJSF/target/generated-sources/annotations/cl/revengers/entities/ResumenProductos_.java
+++ VacaJSF/target/generated-sources/annotations/cl/revengers/entities/ResumenProductos_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-16T10:45:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-18T01:46:13")
 @StaticMetamodel(ResumenProductos.class)
 public class ResumenProductos_ { 
 
--- VacaJSF/target/generated-sources/annotations/cl/revengers/entities/Trabajador_.java
+++ VacaJSF/target/generated-sources/annotations/cl/revengers/entities/Trabajador_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-16T10:45:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-18T01:46:13")
 @StaticMetamodel(Trabajador.class)
 public class Trabajador_ { 
 
